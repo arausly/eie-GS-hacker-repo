5a029d783ca80914601b14696804d7b03cfbfb25
arausly <arausid@yahoo.com>
2018-02-28 14:06:33 +0100

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,5 +1,7 @@
 // Javascript --> solutions
 // feel free to improve.
+// Disclaimer: These are not the questions coming out. If they do, please try not to copy-paste, as that might end up really messed up. 
+// wish you all success.
 
 // /hacker rank series/#1
 // [[daniel,80],[femi,78],[joy,79],[ojo,98]]
